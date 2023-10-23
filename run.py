9b972435e363525b12e94e1f8f42a1c4e413bdca
redfoxofwealden <>
2023-10-23 13:09:44 +0000

    create clear screen function; docs: create layout for documentation

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+class Char:
+    clear_screen = '\x9B3J\x9B;H\x9B0J'
+
+def clear_screen():
+    print(Char.clear_screen, end='')
+
+def test():
+    for c in range(20):
+        print('lots of messages')
+
+    input('Press return to continue\n')
+
+    clear_screen()
+
+    print('Screen should be clear')
+
+test()

fc7713fc60b5a13a3d9ab339bec8ad5900099193
redfoxofwealden <>
2023-10-24 13:29:22 +0000

    add color format command; add restore command to Color class; fix: display in Title class

--- run.py
+++ run.py
@@ -7,18 +7,13 @@ class Char:
     '''
     _clear_screen = '\x9B3J\x9B;H\x9B0J'
 
-    hit = '\u2731'
-    miss = '-'
-    ship = 'S'
-    unknown = '\u25CF'
+    HIT = '\u2731'
+    MISS = '-'
+    SHIP = 'S'
+    UNKNOWN = '\u25CF'
 
     def clear_screen():
         print(Char._clear_screen, end='')
-
-# Constant for foreground colour initial setting
-FG_COLOR = int(82)
-# Constant for background colour initial setting
-BG_COLOR = int(0)
 
 class Color:
     '''
@@ -27,16 +22,35 @@ class Color:
 
     Manages 256 color for setting and retrieval.
     '''
-    _template_background = '\x9B48;5;{}m'
-    _template_foreground = '\x9B38;5;{}m'
+
+    _FOREGROUND_COLOR = int(82) # light green
+    _BACKGROUND_COLOR = int(0)  # black
+
+    _TEMPLATE_BACKGROUND = '\x9B48;5;{}m'
+    _TEMPLATE_FOREGROUND = '\x9B38;5;{}m'
+    _RESET = '\x9B0m'
 
     _current_background = ''
     _current_foreground = ''
+    _current_background_int = -1
+    _current_foreground_int = -1
+    
+    @staticmethod
+    def format(string, fg_color=None, bg_color=None):
+        fg_string = ''
+        bg_string = ''
 
-    _current_background_int = 0
-    _current_foreground_int = 0
+        if fg_color is not None:
+            fg_string = Color._TEMPLATE_FOREGROUND.format(int(fg_color))
+            
+        if bg_color is not None:
+            bg_string = Color._TEMPLATE_BACKGROUND.format(int(bg_color))
+        
+        format_string = bg_string + fg_string + string
+        format_string += Color._current_background
+        format_string += Color._current_foreground
 
-    _reset = '\x9B0m'
+        return str(format_string)
     
     @staticmethod
     def get_background():
@@ -48,13 +62,28 @@ class Color:
 
     @staticmethod
     def reset():
-        print(Color._reset, end='')
+        '''
+        Restore terminal colors to its system defaults
+        '''
+        print(Color._RESET, end='')
+        Color._current_background = ''
+        Color._current_foreground = ''
+        Color._current_background_int = -1
+        Color._current_foreground_int = -1
+
+    @staticmethod
+    def restore():
+        if Color._current_background != '':
+            print(Color._current_background, end='')
+               
+        if Color._current_foreground != '':
+            print(Color._current_foreground, end='')
 
     @staticmethod
     def set_background(background_color):
         Color._current_background_int = int(background_color)
         Color._current_background = str(
-            Color._template_background.format(
+            Color._TEMPLATE_BACKGROUND.format(
                 int(background_color))
         )
         print(Color._current_background, end='')
@@ -63,22 +92,23 @@ class Color:
     def set_foreground(foreground_color):
         Color._current_foreground_int = int(foreground_color)
         Color._current_foreground = str(
-            Color._template_foreground.format(
+            Color._TEMPLATE_FOREGROUND.format(
                 int(foreground_color))
         )
         print(Color._current_foreground, end='')
+        return Color
 
     @staticmethod
     def init():
         Color.reset()
-        Color.set_background(BG_COLOR)
-        Color.set_foreground(FG_COLOR)
+        Color.set_background(Color._BACKGROUND_COLOR)
+        Color.set_foreground(Color._FOREGROUND_COLOR)
 
 class Title:
     '''
     Displaying the title of the game.
     '''
-    _title_name = str('''
+    _title_name = str(r'''
                       ___        _                         
                      / _ \      | |                        
                     / /_\ \_ __ | |_ ___  _ __   _____   __
@@ -91,7 +121,7 @@ class Title:
               | ___ \     | | | | | |         | |   (_)          
               | |_/ / __ _| |_| |_| | ___  ___| |__  _ _ __  ___ 
               | ___ \/ _` | __| __| |/ _ \/ __| '_ \| | '_ \/ __|
-              | |_/ / (_| | |_| |_| |  __/\__ \ | | | | |_) \__ \\
+              | |_/ / (_| | |_| |_| |  __/\__ \ | | | | |_) \__ \
               \____/ \__,_|\__|\__|_|\___||___/_| |_|_| .__/|___/
                                                       | |        
                                                       |_|          
@@ -105,5 +135,7 @@ def test():
     Color.init()
     Char.clear_screen()
     Title.display()
+
+    print(Color.format(' This is red on white background ', 1, 15))
 
 test()
